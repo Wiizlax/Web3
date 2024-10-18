37b77b4fd44f27d904fc02d1872a6ad0bb2a436d
Tom Simonis <tom.simonis@student.vinci.be>
2024-10-18 16:02:56 +0200

    backend server

---
+++ exercises/module4/4.2/src/App.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+
+const App = () => {
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '0470123456' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault(); // Empêche le rafraîchissement de la page
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} already exists in the phonebook!`);
+      return; 
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName('');
+    setNewNumber('');
+  };
+
+  return (
+    <div className='container'>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+                  value={newName}
+                  onChange={handleNameChange}
+                  />
+        </div>
+        <div>
+          number: <input 
+                  value={newNumber}
+                  onChange={handleNumberChange}
+                  />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name} {person.number}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
