4b09de52ac9b88469cd486d78e5131b913cc701d
Tom Simonis <tom.simonis@student.vinci.be>
2024-10-22 16:23:52 +0200

    module6 - 6.1

---
+++ exercises/module6/6.1/mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length !== 3 && process.argv.length !== 5) {
+  console.log("Pas le bon nombre d'arguments");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://Wiizlax:${password}@cluster0.yhenn.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+if (process.argv.length === 5) {
+  const phonebookSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+  });
+
+  const Contact = mongoose.model("Contact", phonebookSchema);
+
+  const contact = new Contact({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  contact.save().then((result) => {
+    console.log(
+      "added " + contact.name + " number " + contact.number + " to phonebook"
+    );
+    mongoose.connection.close();
+  });
+} else {
+    const phonebookSchema = new mongoose.Schema({
+        name: String,
+        number: String,
+      });
+    
+    const Contact = mongoose.model("Contact", phonebookSchema);
+    console.log("Phonebook:")
+    Contact.find({}).then((result) => {
+        result.forEach((contact) => {
+          console.log(contact.name + ' ' + contact.number);
+        });
+        mongoose.connection.close();
+      });
+}
